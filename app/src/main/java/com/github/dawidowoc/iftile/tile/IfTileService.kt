36fa9704e5b885eb769fbf2604ff0eee74c92771
Dawid Owoc <dawidowoc6@gmail.com>
2021-08-27 17:59:46 +0200

    Add showing current date and time in IF tile

--- app/src/main/java/com/github/dawidowoc/iftile/tile/IfTileService.kt
+++ app/src/main/java/com/github/dawidowoc/iftile/tile/IfTileService.kt
@@ -3,22 +3,27 @@ package com.github.dawidowoc.iftile.tile
 import androidx.wear.tiles.*
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
+import java.time.Duration
+import java.util.Date
 
 private const val RESOURCES_VERSION = "1"
+private val REFRESH_INTERVAL = Duration.ofMinutes(1)
 
 class IfTileService : TileProviderService() {
     override fun onTileRequest(requestParams: RequestBuilders.TileRequest):
             ListenableFuture<TileBuilders.Tile> {
         return Futures.immediateFuture(
-            TileBuilders.Tile.builder().setResourcesVersion(RESOURCES_VERSION).setTimeline(
-                TimelineBuilders.Timeline.builder().addTimelineEntry(
-                    TimelineBuilders.TimelineEntry.builder().setLayout(
-                        LayoutElementBuilders.Layout.builder().setRoot(
-                            LayoutElementBuilders.Text.builder().setText("Hello!")
+            TileBuilders.Tile.builder().setResourcesVersion(RESOURCES_VERSION)
+                .setFreshnessIntervalMillis(REFRESH_INTERVAL.toMillis())
+                .setTimeline(
+                    TimelineBuilders.Timeline.builder().addTimelineEntry(
+                        TimelineBuilders.TimelineEntry.builder().setLayout(
+                            LayoutElementBuilders.Layout.builder().setRoot(
+                                LayoutElementBuilders.Text.builder().setText(Date().toString())
+                            )
                         )
                     )
-                )
-            ).build()
+                ).build()
         )
     }
 
