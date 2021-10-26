e59f3b8bcf2aec346fe5ddb2136147dee3690cde
Dawid Owoc <dawidowoc6@gmail.com>
2021-10-26 20:56:01 +0200

    Refactor scheduling API

--- app/src/main/kotlin/com/github/dawidowoc/iftile/notification/NotificationScheduler.kt
+++ app/src/main/kotlin/com/github/dawidowoc/iftile/notification/NotificationScheduler.kt
@@ -5,7 +5,7 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import java.util.Date
+import java.util.Calendar
 
 data class Notification(val title: String, val text: String)
 
@@ -26,7 +26,7 @@ class NotificationScheduler(private val context: Context) {
     private var nextRequestCode = 0
 
     @Synchronized
-    fun scheduleDailyNotification(notification: Notification, startDate: Date) {
+    fun scheduleDailyNotification(notification: Notification, hour: Int, minute: Int) {
         if (nextRequestCode == Int.MAX_VALUE) {
             throw IllegalStateException(
                 "Too much scheduled daily notifications - max " + Int.MAX_VALUE
@@ -39,15 +39,24 @@ class NotificationScheduler(private val context: Context) {
 
         val pendingIntent = PendingIntent.getBroadcast(context, nextRequestCode++, intent, 0)
 
+
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.setRepeating(
             AlarmManager.RTC_WAKEUP,
-            startDate.toInstant().toEpochMilli(),
+            getNextOccuranceTimestampInMillis(hour, minute),
             AlarmManager.INTERVAL_FIFTEEN_MINUTES,
             pendingIntent
         )
 
         pendingIntentsList.add(pendingIntent)
+    }
+
+    private fun getNextOccuranceTimestampInMillis(hour: Int, minute: Int): Long {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, hour)
+        calendar.set(Calendar.MINUTE, minute)
+        calendar.set(Calendar.SECOND, 0)
+        return calendar.timeInMillis
     }
 
     @Synchronized
